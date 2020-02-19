1cb4d115d7ed55a137796024b1887308595a3d64
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2020-02-19 23:28:02 +0100

    feat: added example to execute with custom config

---
+++ 02_external/index.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = "Hola mundo desde Webpack!";
--- 02_external/webpack.config.js
+++ 02_external/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'index.js'),
+  mode: 'development',
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
