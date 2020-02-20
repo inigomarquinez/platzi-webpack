272369fa7621a03346abe8f070940fa7e6329061
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2020-02-20 20:50:25 +0100

    feat: added example for running dev server

--- 04_css-style-loader/webpack.config.js
+++ 04_css-style-loader/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  entry: {
+    home: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  },
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    filename: 'js/[name].js'
   },
   module: {
     rules: [
